a6b2719ebe65a6d494d376144d31dfe84a9dcc62
ferra2391 <ferra2391@gmail.com>
2025-06-16 15:04:51 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const URL_WEB_APP = "https://script.google.com/macros/s/AKfycbybSXuBklZxyoyvo-FvV-UZ280WBygR8wMRTAO97dl8h6_g8-_VjDtBa2uPrVOf2nmI6Q/exec";
+
+function login() {
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+
+  if (user === "mario" && pass === "1234") {
+    document.getElementById("form").style.display = "block";
+  } else {
+    alert("Credenziali errate");
+  }
+}
+
+function invia() {
+  const data = document.getElementById("data").value;
+  const operaio = document.getElementById("operaio").value;
+  const cantiere = document.getElementById("cantiere").value;
+  const lavorazione = document.getElementById("lavorazione").value;
+
+  const dati = {
+    data,
+    operaio,
+    user: "mario",
+    cantiere,
+    lavorazione
+  };
+
+  fetch(URL_WEB_APP, {
+    method: "POST",
+    body: JSON.stringify(dati),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(r => r.text())
+    .then(txt => alert("Dati inviati con successo!"))
+    .catch(err => alert("Errore invio dati"));
+}
