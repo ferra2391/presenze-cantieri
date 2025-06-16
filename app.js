536d17bddfd46ee36310f6fa79e0e08e7dc0855e
ferra2391 <ferra2391@gmail.com>
2025-06-16 15:10:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const URL_WEB_APP = "https://script.google.com/macros/s/AKfycbybSXuBklZxyoyvo-FvV-UZ280WBygR8wMRTAO97dl8h6_g8-_VjDtBa2uPrVOf2nmI6Q/exec";
+const URL_WEB_APP = "https://script.google.com/macros/s/AKfycbybSXuBklZxyoyvo-FvV-UZ280WBygR8wMRTAO97dl8h6_g8-_VjDtBa2uPrVOf2nmI6Q/exec"; // Inserisci qui l'URL copiato da Apps Script
 
 function login() {
   const user = document.getElementById("user").value;
@@ -27,12 +27,12 @@ function invia() {
 
   fetch(URL_WEB_APP, {
     method: "POST",
+    mode: "no-cors", // IMPORTANTE: abilita invio anche da GitHub Pages
     body: JSON.stringify(dati),
     headers: {
       "Content-Type": "application/json",
     },
   })
-    .then(r => r.text())
-    .then(txt => alert("Dati inviati con successo!"))
+    .then(() => alert("Dati inviati (risposta non leggibile in no-cors)"))
     .catch(err => alert("Errore invio dati"));
 }
